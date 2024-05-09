6036695b459ddc2f6c38d12ca988752644471231
leoGlez01 <lworkgonzalez01@gmail.com>
2024-05-08 21:47:23 -0400

    Adding new Readme

--- README.md
+++ README.md
@@ -1,25 +1,13 @@
 <h1>Encrypted Code - L0123 Algorithm</h1>
 
-> [!NOTE]
-pip install encryptedcode
-
-### INFO:
-This package is imported absolutely in your .py files, for example: 'from encryptedcode.encrypted_code import encode', and contains two functions, one for encoding called encode() and another for decoding called decode() using the new L0123 algorithm created by Ing Software <a href="https://leoglez.vercel.app/">Leandro Gonzalez Espinosa.</a>
+> [!INFO]
+EncryptedCode is a python library accessible to everyone that is under development where I use a new encryption algorithm created by &copy; Software Engineer <a href="https://leoglez.vercel.app/">Leandro Gonzalez Espinosa.</a> and named L0123.
 
 > [!IMPORTANT]
-When using this library for the first time on your PC or virtual environment, it will ask you to provide a secret phrase
-You must remember or save this phrase since in order to use the decode function you will need the phrase
-for security reasons for you!!!
+pip install encryptedcode
+
 
 ### USAGE
-A simple example of how to use the library is the following:
-
-> [!NOTE]
-When you use this library for the first time in your project, it will ask you for a secret phrase which will be hidden from any user from the Windows, Mac or Linux file explorer.
+This package is imported absolutely in your .py files, for example: 'from encryptedcode.encrypted_code import encode', and contains two functions, one for encoding called encode() and another for decoding called decode() using the new L0123 algorithm. A simple example of how to use the library is the following:
 
 <img src="src/usage.png" style="aspect-ratio:16/10"></img>
-
-> [!WARNING]
-After defining your secret phrase for the first time in your project, you will not have to re-enter it to encode the strings but as time to use the decoding function later it will ask you for the secret phrase that you defined previously. Which is located in your root directory in case you have forgotten it.
-
-<img src="src/encrypt.png" style="aspect-ratio:16/10"></img>
--- encryptedcode/base.py
+++ encryptedcode/base.py
@@ -1,89 +1,39 @@
-import random
-import os
-import platform
 
-SECRET_KEY_FILE = 'secret_key.txt'
+def get_key():
+    key = 'xD'
 
-if platform.system() != 'Windows':
-    SECRET_KEY_FILE = '.' + SECRET_KEY_FILE
+    return key
 
-def get_secret_key():
-    if os.path.exists(SECRET_KEY_FILE):
-        with open(SECRET_KEY_FILE, 'r') as f:
-            secret_key = f.read()
-    else:
-        secret_key = input("Type your secret phrase, please don't share it!")
-        with open(SECRET_KEY_FILE, 'w') as f:
-            f.write(secret_key)
-        
-        if platform.system() == 'Windows':
-            import ctypes
-            FILE_ATTRIBUTE_HIDDEN = 0x02
-            ctypes.windll.kernel32.SetFileAttributesW(SECRET_KEY_FILE, FILE_ATTRIBUTE_HIDDEN)
+mapping = {
+    '''
+    "message":"Your map items here"
+    '''
+}
 
-    return secret_key
-
+def xor_cipher(input_string, key):
+    return ''.join(chr(ord(c) ^ ord(k)) for c, k in zip(input_string, key * len(input_string)))
 
 def encode(cadena):
-    mapping = {
-        '''Here your Map Items'''
-    }
+    xor_cadena = xor_cipher(cadena, get_key())
 
     nueva_cadena = ""
-    for caracter in cadena:
+    for caracter in xor_cadena:
         if caracter in mapping:
             nueva_cadena += mapping[caracter]
         else:
             nueva_cadena += caracter
-    
-    selfkey = get_secret_key()
 
-    encoded_key = ""
-    for char in selfkey:
-        if char in mapping:
-            encoded_key += mapping[char]
-        else:
-            encoded_key += char
-
-
-    secret_key = (
-        random.choice("df")
-        + random.choice("hj")
-        + random.choice("$^")
-        + random.choice("%")
-        + random.choice("#@")
-        + random.choice("!?")
-        + random.choice("-_")
-        + random.choice("&f")
-        + random.choice("vb")
-        + "."
-        + "L0123"
-    )
-
-    encrypted = nueva_cadena + "." + secret_key+"."+ encoded_key
-    return encrypted
-
+    return nueva_cadena
 
 def decode(cadena):
-    map = {
-        '''Here your Map Items'''
-    }
-    map_inv = {v: k for k, v in map.items()}
-    nueva_cadena = ""
+    map_inv = {v: k for k, v in mapping.items()}
+    xor_cadena = ""
     for caracter in cadena:
         if caracter in map_inv:
-            nueva_cadena += map_inv[caracter]
-        elif caracter == ".":
-            break
+            xor_cadena += map_inv[caracter]
         else:
-            nueva_cadena += caracter
+            xor_cadena += caracter
 
-    secret = input("Type your secret phrase for decode please!")
+    decrypted_cadena = xor_cipher(xor_cadena, get_key())
 
-    if os.path.exists(SECRET_KEY_FILE):
-        with open(SECRET_KEY_FILE, 'r') as f:
-            secretfile = f.read()
-            if secret != secretfile:
-                return ("This phrase is wrong and don't match whit the saved one.")
-            else:
-                return nueva_cadena
+    return decrypted_cadena
